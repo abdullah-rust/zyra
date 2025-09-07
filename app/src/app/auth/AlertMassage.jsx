51e98c5c3cc6ad415853c441066cf8a142ccfe07
Abdullah Riaz <ariaz7556@gmail.com>
2025-09-07 06:47:45 -0700

    next

--- app/src/app/auth/AlertMassage.jsx
+++
@@ -1,5 +0,0 @@
-import styles from "./Alert.module.css";
-
-export default function Alert({ message, type = "info" }) {
-  return <div className={`${styles.alert} ${styles[type]}`}>{message}</div>;
-}
